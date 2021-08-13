20226331cc514b14d8ef2b27c82ee79b24e3bb78
krish1010 <krish.krishanu10@gmail.com>
2021-08-13 11:38:38 +0530

    added tables

---
+++ database/models/tables/tasks.py
@@ -0,0 +1,58 @@
+from database.db import db
+from database.models.enums.task_status import TaskStatus
+from datetime import datetime
+
+
+class Task(db.Model):
+    __tablename__ = 'task'
+
+    idx = db.Column(db.String, primary_key=True)
+
+    manager_id = db.Column(db.String, db.ForeignKey('users.idx'))
+    employee_id = db.Column(db.String, db.ForeignKey('users.idx'))
+
+    title = db.Column(db.String)
+    description = db.Column(db.String)
+    created_at = db.Column(db.DateTime, default=datetime.now, nullable=False)
+    taken_on = db.Column(db.DateTime)
+    completed_on = db.Column(db.DateTime)
+    status = db.Column(db.Enum(TaskStatus), default='OPEN')
+
+    @classmethod
+    def get(cls, pk):
+        return cls.query.get(pk)
+
+    @classmethod
+    def get_by_uid(cls, idx: str):
+        task = cls.query.filter_by(idx=idx).first()
+        return task
+
+    @classmethod
+    def create(cls, **kwargs):
+        instance = cls(**kwargs)
+        return instance.save()
+
+    @classmethod
+    def get_all_tasks(cls):
+        return cls.query.all()
+
+    def update(self, commit=True, **kwargs):
+        for attr, value in kwargs.items():
+            setattr(self, attr, value)
+        if commit:
+            self.save()
+        return self
+
+    def save(self, commit=True):
+        db.session.add(self)
+        if commit:
+            try:
+                db.session.commit()
+            except Exception:
+                db.session.rollback()
+                raise
+        return self
+
+    def delete(self, commit=True):
+        db.session.delete(self)
+        return commit and db.session.commit()
