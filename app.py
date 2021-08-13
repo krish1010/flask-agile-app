62411c1be7e0094dbe184def11058507210efc7f
krish1010 <krish.krishanu10@gmail.com>
2021-08-13 11:23:50 +0530

    added app.py

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from database.db import db
+
+from blueprints.users import USERS_BLUEPRINT
+from blueprints.tasks import TASKS_BLUEPRINT
+
+app = Flask(__name__)
+app.config.from_pyfile('config/settings.staging.cfg')
+db.init_app(app)
+
+app.register_blueprint(USERS_BLUEPRINT, url_prefix='/users')
+app.register_blueprint(TASKS_BLUEPRINT, url_prefix='/tasks')
+
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return 'Hello World!'
+
+
+if __name__ == '__main__':
+    app.run()
