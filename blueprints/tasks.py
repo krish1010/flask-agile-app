f00f053a7450e47ccc04debdf275efc6ba9e1ced
krish1010 <krish.krishanu10@gmail.com>
2021-08-27 23:21:37 +0530

    added /history endpoint

--- blueprints/tasks.py
+++ blueprints/tasks.py
@@ -9,11 +9,11 @@ CREATE_TASK = 'create_task.html'
 @TASKS_BLUEPRINT.route('/', methods=['GET'])
 def index():
     if 'user_idx' not in session:
-        # flash('You are already logged in', 'info')
         return redirect(url_for('users.login'))
     all_tasks = get_all_tasks()
+    filtered_tasks = filter(lambda x: x.status.value != 'approved', all_tasks)
     context = {
-        'all_tasks': all_tasks
+        'all_tasks': filtered_tasks
     }
     return render_template('display_tasks.html', **context)
 
@@ -21,7 +21,6 @@ def index():
 @TASKS_BLUEPRINT.route('/create', methods=['GET', 'POST'])
 def create():
     if 'user_idx' not in session:
-        # flash('You are already logged in', 'info')
         return redirect(url_for('users.login'))
     if request.method == 'GET':
         return render_template(CREATE_TASK, **{'source': 'tasks.create'})
@@ -31,7 +30,7 @@ def create():
         taken_on = request.form.get('taken-on')
         completed_on = request.form.get('completed-on')
 
-        create_task(title, description, taken_on, completed_on)
+        create_task(title, description, taken_on, completed_on, session.get('user_idx'))
 
         return render_template(CREATE_TASK, **{'source': 'tasks.create'})
 
@@ -41,30 +40,20 @@ def update(idx):
     task = get_task_by_id(idx)
     if request.method == 'GET':
         return render_template(CREATE_TASK, **{'source': 'tasks.update', 'idx': idx, 'task': task})
-    if request.method == 'POST':
-        if task:
-            if session.get('user_type') == 'manager':
-                title = request.form.get('title')
-                description = request.form.get('description')
-                taken_on = request.form.get('taken-on')
-                completed_on = request.form.get('completed-on')
-                status = request.form.get('status')
-                if status == 'subscribed':
-                    employee_id = session.get('user_idx')
-                    task.update(status=TaskStatus(status), user_id=employee_id)
+    if request.method == 'POST' and task:
+        if session.get('user_type') == 'manager':
+            title = request.form.get('title')
+            description = request.form.get('description')
+            taken_on = request.form.get('taken-on')
+            completed_on = request.form.get('completed-on')
+            status = request.form.get('status')
 
-                update_task(task=task, title=title, description=description, taken_on=taken_on,
-                            completed_on=completed_on, status=status)
-            else:
-                status = request.form.get('status')
-                if status == 'subscribed':
-                    employee_id = session.get('user_idx')
-                    task.update(status=TaskStatus(status), user_id=employee_id)
-                else:
-                    task.update(status=TaskStatus(status))
-            return redirect(url_for('tasks.index'))
+            update_task(task=task, title=title, description=description, taken_on=taken_on,
+                        completed_on=completed_on, status=status)
         else:
-            ...
+            task.update(status=TaskStatus(request.form.get('status')), developer_user_id=session.get('user_idx'))
+
+        return redirect(url_for('tasks.index'))
 
 
 @TASKS_BLUEPRINT.route('/delete/<idx>', methods=['GET'])
@@ -72,3 +61,16 @@ def delete(idx):
     task = get_task_by_id(idx)
     task.delete()
     return redirect(url_for('tasks.index'))
+
+
+@TASKS_BLUEPRINT.route('/history', methods=['GET'])
+def history():
+    if 'user_idx' not in session:
+        return redirect(url_for('users.login'))
+    all_tasks = get_all_tasks()
+    filtered_tasks = filter(lambda x: session.get('username') in (x.created_by_username, x.developer_username), all_tasks)
+    context = {
+        'all_tasks': filtered_tasks,
+        'source': 'history'
+    }
+    return render_template('display_tasks.html', **context)
